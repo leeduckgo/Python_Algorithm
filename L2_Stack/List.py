1dc4f328939172854c78f03bada02c6004bfa548
hitchhacker <albertschr@163.com>
2017-03-22 16:55:38 +0800

    Add files via upload

---
+++ L2_Stack/List.py
@@ -0,0 +1,24 @@
+#-*-coding:utf-8-*-
+aList=[]
+#列表的定义很简单，变量名=[]即可
+
+aList=[1,2,3]
+#如果要赋予初值的话，变量名=[值A,值B,值C……]即可。
+
+aList.append('dog')
+#append(item)函数添加「值」(东西)到列表(盒子)中。
+
+print len(aList)
+#len(listname)函数输出列表的长度（盒子数目）.
+
+print aList[2]
+#输出编号为2的盒子中的东西，编号从0开始。
+
+print aList.pop(2)
+#pop(n)函数去除编号为n的盒子中的东西。
+
+print aList
+#直接print listName打印列表
+
+
+
